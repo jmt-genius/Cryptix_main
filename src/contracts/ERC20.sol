e15f1e05ab5c9b98248527e5e2441b561587ae2f
Akkilesh-A <akkilalagar05@gmail.com>
2024-08-06 23:41:34 +0530

    setup redux

--- src/contracts/ERC20.sol
+++ src/contracts/ERC20.sol
@@ -1,26 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC20 {
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
+import "@openzepplin/contracts/token/ERC20/IERC20.sol";
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balanceOf[msg.sender] = _initialSupply;
-    }
-
-    function transfer(address _to, uint256 _value) external returns (bool) {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+contract ERC20Token is ERC20 {
+    constructor() ERC20("@theblockchaincoders", "TBC") {
+        _mint(msg.sender, 50000*18);
     }
 }
